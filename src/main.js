c4582be69316ae052ebc08038210f5208d88f0c2
Anthony Rojas <anthonyrojas@anthonys-mbp.attlocal.net>
2018-06-24 00:47:20 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueScrollReveal from 'vue-scroll-reveal'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App)
