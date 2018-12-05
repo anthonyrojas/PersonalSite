da63da5a540bebe15bca1f55215f27a319e622dc
Anthony Rojas <anthony.rojas@freshrealm.co>
2018-12-04 16:55:27 -0800

    styling progress on skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -12,8 +12,8 @@ class Skills extends Component{
                         return(
                             <div className='row' key={skillList.id}>
                                 <div className='col-9 col-md-9 col-lg-8'>
-                                    <div className='paper'>
-                                        <h3>{skillList.id}</h3>
+                                    <div className='paper skill-list-container'>
+                                        <h3 className='skill-list-title'>{skillList.title}</h3>
                                         {
                                             skillList.skillsList.map((skill)=>(
                                                 <SkillsItem skill={skill} key={skill.id}/>
--- src/Components/Skills/SkillsItem.js
+++ src/Components/Skills/SkillsItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Skills.css';
 const SkillsItem = (props) => (
-    <div className='skill-item'>
-        <p>{props.skill.title}</p>
-        {console.log(props.skill)}
+    <div className='skill-item-container'>
+        <div className='skill-item'>
+            <img className='skill-image' src={props.skill.imageSource} alt={props.skill.title}/>
+            <p className='skill-item-name'>{props.skill.title}</p>
+        </div>
     </div>
 );
 export default SkillsItem;
